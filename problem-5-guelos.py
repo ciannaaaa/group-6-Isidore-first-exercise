fd77e1055b82bd44e1fad75ed562e45eca6f1fd3
Yohan <guelosjohnyohan@gmail.com>
2026-07-09 19:32:04 +0800

    problem-5-guelos.py

---
+++ problem-5-guelos.py
@@ -0,0 +1,13 @@
+1 | #Problem 5
+2 | #Name: John Yohan J. Guelos
+3 | #Grade: 11 - St. ISIDORE(ROBOTICS)
+4 | 
+5 | name = "John Yohan J. Guelos"
+6 | age = "16"
+7 | subject = "Math"
+8 |
+9 | print("====== My Info Card ======")
+10| print(f"Name    : {name}")
+11| print(f"Age     : {age}")
+12| print(f"Subject : {subject}")
+13| print("==========================")
