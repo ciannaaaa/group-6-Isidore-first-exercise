814873844042ea65e3a98bf1830e902c2c65b552
Yohan <guelosjohnyohan@gmail.com>
2026-07-09 19:56:48 +0800

    problem-5-guelos.py

--- problem-5-guelos.py
+++ problem-5-guelos.py
@@ -1,13 +1,13 @@
-1 | #Problem 5
-2 | #Name: John Yohan J. Guelos
-3 | #Grade: 11 - St. ISIDORE(ROBOTICS)
-4 | 
-5 | name = "John Yohan J. Guelos"
-6 | age = "16"
-7 | subject = "Math"
-8 |
-9 | print("====== My Info Card ======")
-10| print(f"Name    : {name}")
-11| print(f"Age     : {age}")
-12| print(f"Subject : {subject}")
-13| print("==========================")
+ #Problem 5
+ #Name: John Yohan J. Guelos
+ #Grade: 11 - St. ISIDORE(ROBOTICS)
+
+ name = "John Yohan J. Guelos"
+ age = "16"
+ subject = "Math"
+
+print("====== My Info Card ======")
+print(f"Name    : {name}")
+print(f"Age     : {age}")
+print(f"Subject : {subject}")
+print("==========================")
