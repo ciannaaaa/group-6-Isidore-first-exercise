0adcd655156f159660c9b6bf9fe1494af3b060ec
ciannaaaa <mciannamarie@gmail.com>
2026-07-09 10:06:20 +0800

    problem-1-montong.py

---
+++ problem-1-montong.py
@@ -0,0 +1,16 @@
+name = "Cianna"
+age = 16
+gpa = 95
+enrolled = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(gpa)
+print(type(gpa))
+
+print(enrolled)
+print(type(enrolled))
